3206d7edd64b8305392b8e23fe57bed41cac5aa7
jpiraguha <jiraguha@gmail.com>
2024-04-10 10:24:39 +0200

    add seats calculation

--- functions/triggers/updateVoteCountsAndResults.js
+++ functions/triggers/updateVoteCountsAndResults.js
@@ -9,7 +9,7 @@ async function updateParties(campaignId, voteData) {
   let totalVotesIncrement = 0;
   let totalClassicVotesIncrement = 0;
   await Promise.all(
-    Object.keys(voteData.parties).forEach(async (partyId) => {
+    Object.keys(voteData.parties).map(async (partyId) => {
       const { count } = voteData.parties[partyId];
       const partyRef = admin.database().ref(`parties/${campaignId}/${partyId}`);
       // Transactionally update the party vote count
@@ -56,6 +56,72 @@ async function updateResult(
   });
 }
 
+// Fonction pour calculer les quotients pour chaque parti
+function calculateQuotients(seats, votes, keyObj) {
+  let quotients = [];
+  Object.entries(votes).forEach(([key, value]) => {
+    for (let i = 1; i <= seats; i++) {
+      quotients.push({ quotient: value[keyObj] / i, partyIndex: key });
+    }
+  });
+  return quotients;
+}
+
+// Fonction pour attribuer les sièges selon la méthode D'Hondt
+function baseDistributeSeats(seats, votes, keyObj) {
+  const quotients = calculateQuotients(seats, votes, keyObj);
+
+  // Tri des quotients du plus grand au plus petit
+  quotients.sort((a, b) => b.quotient - a.quotient);
+
+  // Initialisation de la répartition des sièges
+  let seatsWon = new Object();
+  // Attribution des sièges
+  for (let i = 0; i < seats; i++) {
+    incrementObject(seatsWon, quotients[i].partyIndex);
+  }
+
+  return seatsWon;
+}
+
+function incrementObject(obj, key) {
+  if (obj[key]) {
+    obj[key] += 1;
+  } else {
+    obj[key] = 1;
+  }
+}
+
+async function distributeSeats(campaignId, seats) {
+  const partiesRef = admin.database().ref(`parties/${campaignId}`);
+  const snapshot = await partiesRef.once('value');
+
+  //if (!snapshot.exists()) {
+  //  return [];
+  //}
+
+  const partiesData = snapshot.val();
+  const valueDistributeSeats = baseDistributeSeats(
+    seats,
+    partiesData,
+    'classicVoteCount'
+  );
+  const classicValueDistributeSeats = baseDistributeSeats(
+    seats,
+    partiesData,
+    'voteCount'
+  );
+  for (const key of Object.keys(partiesData)) {
+    const partyRef = admin.database().ref(`parties/${campaignId}/${key}`);
+    await partyRef.child('seatCount').transaction(() => {
+      return valueDistributeSeats[key] || 0;
+    });
+    await partyRef.child('classicSeatCount').transaction(() => {
+      return classicValueDistributeSeats[key] || 0;
+    });
+  }
+}
+
 exports.updateVoteCountsAndResults = functions
   .region('europe-west1')
   .firestore.document('votes/{voteId}')
@@ -72,4 +138,5 @@ exports.updateVoteCountsAndResults = functions
       totalVotesIncrement,
       totalClassicVotesIncrement
     );
+    await distributeSeats(campaignId, 125);
   });
